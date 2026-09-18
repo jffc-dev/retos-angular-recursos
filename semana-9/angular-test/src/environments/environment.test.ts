a8ea6470abe51d5aa45f3e65709b4c95084c8119
Javier Flores <jffc.dev@gmail.com>
2026-09-18 02:42:17 -0500

    agregando cambio de prueba

--- semana-9/angular-test/src/environments/environment.test.ts
+++ semana-9/angular-test/src/environments/environment.test.ts
@@ -3,6 +3,6 @@ import { EnvInterface } from "./env.type";
 export const environment: EnvInterface = {
   production: false,
   appEnvironment: 'TEST',
-  supabaseApi: 'http://ssaf',
+  supabaseApi: 'http://ssaf1',
   supabaseKey: 'asdfasdf',
 };
